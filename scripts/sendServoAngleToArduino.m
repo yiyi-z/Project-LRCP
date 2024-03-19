640ba619dc1f805a0a86850c49bfa82aa57cbc4c
vishal14022002 <vishal1402.r@gmail.com>
2024-03-19 11:59:52 -0400

    Servo angles code change

--- scripts/sendServoAngleToArduino.m
+++ scripts/sendServoAngleToArduino.m
@@ -8,14 +8,15 @@ numSamples = 5;
 % test for same angles
 AngleToBeRepeat = randi([0, 180], numSamples, 1);
 servoAngleAllTime = repmat(AngleToBeRepeat, 1, 1, 6);
+% Add a row of zeros at the end
+servoAngleAllTime(end+1, :) = zeros(1, 6);
 disp("all angles: " + servoAngleAllTime);
 
 %test for different angles
  %servoAngleAllTime = rand(numSamples, 1, 6) * 180.0;
 % disp("all angles: " + servoAngleAllTime);
 
- 
-%% 
+%%
 % Connect to Arduino
 arduinoObj = serialport("COM7", 9600); % Update COM3 to your Arduino's port
 
@@ -30,19 +31,17 @@ for i=1:numSamples
     numbers = servoAngles;
     configure = [0,0,0,0,0,0];
     % Convert the array to a string format: "12,34"
-    configureString = strjoin(string(configure), ",");
-    numbersString = strjoin(string(numbers), ",");
+    %configureString = strjoin(string(configure), ",");
+    numberString = strjoin(string(configure), ",");
     
     % Send the string to Arduino
-    writeline(arduinoObj, configureString);
+    writeline(arduinoObj, numberString);
     
     % Wait and read confirmation from Arduino
     confirmation = readline(arduinoObj);
     
     % Display the confirmation
     disp(['Confirmation from Arduino: ', confirmation]);
-    
-
 end
 
 % Clean up
